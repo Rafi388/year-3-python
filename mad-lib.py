b729f3e7a0a7ac8db28b410287e203b2e012080e
Rafi388 <7267@trinity.croydon.sch.uk>
2025-03-03 11:28:28 +0000

    Add files via upload

---
+++ mad-lib.py
@@ -0,0 +1,15 @@
+print("WELCOME TO MAD-LIB")
+print("PLEASE ANSWER THE FOLLOWING QUESTIONS")6
+print("")
+
+question5 = input("what is your name")
+question0 = input ("what is your nickname")
+question1 = input("something that describes you e.g short/fat/has a massive forehead")
+question2 = input("something you could do on days on end watch movies/eating")
+question3 = input("your faviroute snack")
+question4 = input("something that describes you e.g angry/exposive/lazy")
+
+print("")
+print("Here is your MAD-LIB")
+print("")
+print(question5,"also known as" ,question0, "is a" ,question1, "short tempered" ,question4, "guy who could easily" ,question2, "days one end whilst stuffing his mouth with rotten" ,question3,)
