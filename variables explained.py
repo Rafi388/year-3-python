6ca8a63d01c51f855adb2afa0d51d82fabd6bc11
Rafi388 <7267@trinity.croydon.sch.uk>
2025-03-10 10:31:58 +0000

    Update variables explained.py

---
+++ variables explained.py
@@ -0,0 +1,28 @@
+#a variable is:
+#a named locatiom...
+#in a computers memory...
+#it stored a value...
+#and the value can change
+
+
+#example:
+name = "dave"
+#the variable is called name
+#the value is "dave"
+#i will now change the value
+name = "alan"
+
+# variables cxan store different...
+#types of data
+#e.g. numbers, letters etc
+
+#the best progremmers
+#make sure the variable name
+#is descriptive
+#e.g.
+team0neYellowCards = 0
+#the variable above has...
+#a very descriptive name
+x = 1
+#this variable has a poor...
+#name!
