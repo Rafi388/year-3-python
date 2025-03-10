6b34c4f565b2d4320d4eb572bafd2a7b94232b24
Rafi388 <7267@trinity.croydon.sch.uk>
2025-03-10 10:45:07 +0000

    Add files via upload

---
+++ variables and imput challange.py
@@ -0,0 +1,34 @@
+name = input("Enter your name>>>")
+print("hello",name)
+
+colour = input("Enter your fav colour>>>")
+print(colour,"thats a nice colour, mine is red")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
