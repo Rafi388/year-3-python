7e2de8a263a5d4894505b7bc5575835e5f7e0774
Rafi388 <7267@trinity.croydon.sch.uk>
2025-03-17 11:22:37 +0000

    Add files via upload

---
+++ 08 selection challenge.py
@@ -0,0 +1,13 @@
+print ("press 1 for 1-player")
+print ("press 2 for 2-player")
+print ("press 3 for Online")
+choice = int (input ( "enter 1, 2 or 3") )
+
+if choice == 1:
+    print ("Thank you for choosing 1-player")
+elif choice == 2:
+    print ("Thank you for choosing 2-player")
+elif choice == 3:
+    print ("Thank you for choosing online")
+else:
+    print("Invalid choice")
